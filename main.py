52363597bf56b11412d8c2b5e8f37ae170670b62
Akash Singh <akashsingh2313@gmail.com>
2020-09-28 16:35:48 +0530

    Connected MongoDB

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from flask import Flask
+import pymongo
+from pymongo import MongoClient
 
-app = Flask(__name__)
+cluster = MongoClient("mongodb+srv://akash:akash@cluster0.zqitl.mongodb.net/<dbname>?retryWrites=true&w=majority")
 
+db = cluster["dbname"]
+collection = db["coll"]
 
-@app.route('/')
-def index():
-    return "hello world"
-
-
-if __name__=="__main__":
-    app.run();
+collection.insert_one({"_id": 0, "name": "akash singh"})
