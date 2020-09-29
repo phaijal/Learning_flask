4c8bcb3665f60615cb4e7acff55991289078ef4c
Akash Singh <akashsingh2313@gmail.com>
2020-09-29 13:00:29 +0530

    Added function to add data in DB using URL

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask import request
 import pymongo
 from pymongo import MongoClient
+import json
+from bson import json_util
 
+app = Flask(__name__)
 cluster = MongoClient("mongodb+srv://akash:akash@cluster0.zqitl.mongodb.net/<dbname>?retryWrites=true&w=majority")
 
 db = cluster["dbname"]
@@ -9,7 +13,7 @@ collection = db["coll"]
 
 #collection.insert_one({"_id": 1, "name": "sadff asfdasf"})
 
-app = Flask(__name__)
+
 
 
 @app.route("/")
@@ -21,5 +25,21 @@ def getval():
         out += f"{s} "
     return out
 
+
+@app.route("/add/<name>")
+def addname(name):
+    collection.insert_one({"name": name})
+    return f"added {name}"
+
+
+@app.route("/addval", methods=["POST"])
+def addval():
+    request_payload = request.json
+    print(request_payload)
+    print("hjh")
+  #  inf = request_payload['info']
+  #  collection.insert_one({"name": "ttt"})
+    return f"added"
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, port=5000, host="0.0.0.0")
